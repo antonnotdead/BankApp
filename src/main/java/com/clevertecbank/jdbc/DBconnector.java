1e5f7a14145d7f8cb0afa4027a948c5fd21ad552
antonnotdead <oneusergav@gmail.com>
2023-08-26 22:38:27 +0300

    Initial commit

--- src/main/java/com/clevertecbank/jdbc/DBconnector.java
+++ src/main/java/com/clevertecbank/jdbc/DBconnector.java
@@ -4,7 +4,37 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBconnector {
-    private final String url = "jdbc:postgresql://localhost:5432/bankapp";
-    private final String user = "postgres";
-    private final String password = "rootroot";
+    private final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
+    private final String URL = "jdbc:postgresql://localhost:5432/bankapp";
+    private final String USER = "postgres";
+    private final String PASSWORD = "rootroot";
+    private static DBconnector instance = null;
+
+
+    private DBconnector(){
+        try {
+            Class.forName(DRIVER_CLASS_NAME);
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    public Connection getConnection() throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        return conn;
+    }
+
+    public static DBconnector getInstance() {
+        DBconnector localInstance = instance;
+        if (localInstance == null) {
+            synchronized (DBconnector.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new DBconnector();
+                }
+            }
+        }
+        return localInstance;
+    }
 }
+
